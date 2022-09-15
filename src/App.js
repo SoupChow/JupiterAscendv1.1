1e59bbf1d5276df2e836da7c110ec29993ce2a46
SouparnaChowdhury <souparna_chowdhury@persistent.com>
2022-09-15 13:31:41 +0530

    MS SSO

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { PageLayout } from "./components/PageLayout";
+import { AuthenticatedTemplate, UnauthenticatedTemplate } from "@azure/msal-react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageLayout>
+    <AuthenticatedTemplate>
+        <p>You are signed in!</p>
+    </AuthenticatedTemplate>
+    <UnauthenticatedTemplate>
+        <p>You are not signed in! Please sign in.</p>
+    </UnauthenticatedTemplate>
+</PageLayout>
   );
 }
 
---
+++ src/authConfig.js
@@ -0,0 +1,21 @@
+export const msalConfig = {
+    auth: {
+      clientId: "a3326704-06e6-4065-b978-0e89969a2d83",
+      authority: "https://login.microsoftonline.com/1f4beacd-b7aa-49b2-aaa1-b8525cb257e0", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+      redirectUri: "http://localhost:3000",
+    },
+    cache: {
+      cacheLocation: "sessionStorage", // This configures where your cache will be stored
+      storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    }
+  };
+  
+  // Add scopes here for ID token to be used at Microsoft identity platform endpoints.
+  export const loginRequest = {
+   scopes: ["User.Read"]
+  };
+  
+  // Add the endpoints here for Microsoft Graph API services you'd like to use.
+  export const graphConfig = {
+      graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
+  };
